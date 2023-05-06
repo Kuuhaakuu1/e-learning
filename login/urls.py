ed86aac03de62305931a68918ccc932a8798b9c1
elgbouri saad <elgbouri.saad@gmail.com>
2023-05-06 22:02:01 +0100

    add magnetList

--- login/urls.py
+++ login/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from login.views import findMagnets,index,home,courses,register
+from login.views import findMagnets,index,home,courses,register,magnetLists
 
 urlpatterns = [
     path('', index),
     path('courses/',courses,name="courses"),
     path('home/',home,name="home"),
     path('register/',register, name="register"),
+    path('magnetLists/',magnetLists, name="magnetLists"),
+
     path('Java/',findMagnets,name="Java"),
    
 ]
