b42ec6aefa22a557490c6c10e6824befbdefcb51
Omar Temsamani <omar.temsamani@emsi-edu.ma>
2023-05-10 14:05:53 +0100

    final ver

--- login/urls.py
+++ login/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from login.views import findMagnets,index,home,courses,register,magnetLists
+from login.views import index,home,courses,register,searchMagnets
 
 urlpatterns = [
     path('', index),
     path('courses/',courses,name="courses"),
     path('home/',home,name="home"),
     path('register/',register, name="register"),
-    path('magnetLists/',magnetLists, name="magnetLists"),
+    # path('magnetLists/',magnetLists, name="magnetLists"),
 
-    path('Java/',findMagnets,name="Java"),
+    # path('Java/',findMagnets,name="Java"),
+    # path('test/<str:param>/',findMagnets,name="courseMagnets"),
+    path('study/', searchMagnets, name='study'),
+    path('magnets/<str:keyword>/', searchMagnets, name='magnets'),
    
 ]
--- login/views.py
+++ login/views.py
@@ -3,32 +3,81 @@ import pymongo
 import string
 from bs4 import BeautifulSoup
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+from .models import MagnetLinks
 
 
 # Create your views here.
 def index(request):
     return render(request, 'login.html')
 
-def searchMagnets(keyword):
+
+# def search(request):
+#     if request.method == 'POST':
+#         keyword = request.POST['keyword']
+
+#         # Connect to the MongoDB database
+#         client = pymongo.MongoClient("mongodb+srv://admin:S0uf14n3_0m4R_$44d@elearning.i6x9053.mongodb.net/test",connectTimeoutMS=60000, 
+#                              socketTimeoutMS=60000)
+#         db = client.get_database("Elearning")
+#         magnet_links = db[f"magnets{keyword}"]
+#         count = db["keywords"].count_documents({"keyword": {"$regex": keyword, "$options": "i"}})
+
+#         # if magnet_links.count() == 0:
+#         if count == 0:
+#             print("No results found in the database")
+#             # If no results found in the database, call the searchMagnets function
+#             searchMagnets(keyword)
+#             magnet_links = db[f"magnets{keyword}"]
+#         context = {'data': magnet_links, 'keyword': keyword}
+#         return render(request, 'magnets.html', context)
+
+#     return render(request, 'search.html')
+
+    # if request.method == 'POST':
+    #     keyword = request.POST['keyword']
+    #     magnets = MagnetLinks.objects.filter(title__icontains=keyword)
+    #     data = []
+    #     for magnet in magnets:
+    #         data.append({'title': magnet.title, 'magnet': magnet.magnet})
+    #     return render(request, 'search_results.html', {'keyword': keyword, 'data': data})
+    # else:
+    #     return render(request, 'search.html')
+
+    # if request.method == 'POST':
+    #     keyword = request.POST['keyword']
+
+    #     magnet_links = MagnetLinks.objects.filter(title__icontains=keyword)
+    #     if not magnet_links:
+    #         searchMagnets(keyword)
+    #         magnet_links = MagnetLinks.objects.filter(title__icontains=keyword)
+
+    #     context = {'data': magnet_links, 'keyword': keyword}
+    #     return render(request, 'magnets.html', context)
+
+    # return render(request, 'search.html')
+    
+def searchMagnets(request):
+    keyword = request.POST.get('keyword')
     client = pymongo.MongoClient("mongodb+srv://admin:S0uf14n3_0m4R_$44d@elearning.i6x9053.mongodb.net/test")
     db = client.get_database("Elearning")
     i=0
     # Make a request to the website
-    url = 'https://rargb.to/search/?search={}&order=seeders&by=DESC'
-    # keyword = input("Enter the keyword: ")
-    keywordCollection = db["keywords"]
-    keyword = string.capwords(keyword)
+    # &order=seeders&by=DESC
+    url = 'https://rargb.to/search/?search={}'
+
     # Define the regular expression pattern to use in the search
     regex_pattern = f".*{keyword}.*"
 
     # Count the number of documents in the collection that contain the search string
-    count = keywordCollection.count_documents({"keyword": {"$regex": regex_pattern}})
+    count = db["keywords"].count_documents({"keyword": {"$regex": regex_pattern}})
+    magnetLinks = db[f"magnets{keyword}"]
 
-    # Check if the count is greater than 0
+    # Check if the count is greater than 0
     if count >=0:
-        keywordCollection.insert_one({"keyword": keyword})
+        db["keywords"].insert_one({"keyword": keyword})
         response = requests.get(url.format(keyword))
 
         ##############################################################################
@@ -39,7 +88,6 @@ def searchMagnets(keyword):
         # Find all the image tags
         courses = soup.find_all('a')
 
-        magnetLinks = db[f"magnets{keyword}"]
 
         for course in courses:
 
@@ -65,9 +113,75 @@ def searchMagnets(keyword):
                                 i+=1
         if(i==0):
             magnetLinks.drop()
-            #Need to handle the error in a better way    
-    # keywordsRead.close()
-    # keywordsWrite.close()
+
+    # Get the data from the database
+    data = []
+    for el in magnetLinks.find():
+        data.append(el)
+
+    # Render the HTML template with the data
+    context = {'keyword': keyword, 'data': data}
+    return render(request, 'magnetLists.html', context)
+
+
+
+# def searchMagnets(keyword):
+#     client = pymongo.MongoClient("mongodb+srv://admin:S0uf14n3_0m4R_$44d@elearning.i6x9053.mongodb.net/test")
+#     db = client.get_database("Elearning")
+#     i=0
+#     # Make a request to the website
+#     url = 'https://rargb.to/search/?search={}&order=seeders&by=DESC'
+#     # keyword = input("Enter the keyword: ")
+#     keywordCollection = db["keywords"]
+#     keyword = string.capwords(keyword)
+#     # Define the regular expression pattern to use in the search
+#     regex_pattern = f".*{keyword}.*"
+
+#     # Count the number of documents in the collection that contain the search string
+#     count = keywordCollection.count_documents({"keyword": {"$regex": regex_pattern}})
+
+#     # Check if the count is greater than 0
+#     if count >=0:
+#         keywordCollection.insert_one({"keyword": keyword})
+#         response = requests.get(url.format(keyword))
+
+#         ##############################################################################
+
+#         # Parse the HTML content
+#         soup = BeautifulSoup(response.content, 'html.parser')
+
+#         # Find all the image tags
+#         courses = soup.find_all('a')
+
+#         magnetLinks = db[f"magnets{keyword}"]
+
+#         for course in courses:
+
+#             if(str(course).find("/torrent")!=-1):
+#                 href = course['href']
+#                 if(course.text!="  "):
+#                     next_anchor = course.find_next('a')
+#                     next_href = next_anchor['href']
+#                     if(next_href.find("other")!=-1):
+
+#                         newurl = 'https://rargb.to/' + str(href)
+#                         response = requests.get(newurl)
+
+#                         # Parse the HTML content
+#                         soup = BeautifulSoup(response.content, 'html.parser')
+
+#                         magnets = soup.find_all('a')
+                        
+#                         for magnet in magnets:
+#                             if(str(magnet).find("magnet:")!=-1):
+#                                 href = magnet['href']
+#                                 magnetLinks.insert_one({"name":course.text, "magnet": str(href)})
+#                                 i+=1
+#         if(i==0):
+#             magnetLinks.drop()
+#             #Need to handle the error in a better way    
+#     # keywordsRead.close()
+#     # keywordsWrite.close()
 
 def findMagnets(request):
     keyword = 'Java'
@@ -77,8 +191,9 @@ def findMagnets(request):
 
     # collection = db.get_collection(collectionname)
     magnetLinks = db[collectionName].find()
-    channel_Id = 'UC8butISFwT-Wl7EV0hUK0BQ'
-    live_url = f'https://www.youtube.com/embed/live_stream?channel={channel_Id}'
+    # channel_Id = 'UC8butISFwT-Wl7EV0hUK0BQ'
+    # live_url = f'https://www.youtube.com/embed/live_stream?channel={channel_Id}'
+    
     # Find all documents in the collection
     # documents = collection.find()
     # query = {'status': 'published'}
@@ -87,7 +202,9 @@ def findMagnets(request):
         # doc = MagnetLinks(document.title, document.magnet)
         # doc.save()
     # d = MagnetLinks.objects.all()
-    return render(request, 'index.html',{'data': magnetLinks, 'keyword' : keyword, 'channel_Id' : live_url})
+    # , 'channel_Id' : live_url
+    return render(request, 'magnetLists.html',{'data': magnetLinks, 'keyword' : keyword})
+
 def home(request):
     # get post data from request
     if request.method == 'POST':
